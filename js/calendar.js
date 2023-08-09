496e4de443927508431c8a467f03ff02cc365306
Stanislav Sidorov <s.sidorov@sovintegra.ru>
2023-08-09 11:11:13 +0300

    Calendar created

--- js/calendar.js
+++ js/calendar.js
@@ -8,6 +8,7 @@ class CustomCalendar{
     this.calHeaderTitle = null;
     this.calDays = null;
     this.days = [
+      
       "Вс",
       "Пн",
       "Вт",
@@ -44,6 +45,7 @@ class CustomCalendar{
     this.calHeader = head_element.querySelector(".calendar_header");
     this.calHeaderTitle = head_element.querySelector(".calendar_header span");
     this.calDays = head_element.querySelector(".cal_days");
+    this.clearSign = head_element.querySelector(".cal_input_clear");
     this.todayTimestamp = Date.now() - (Date.now() % this.oneDay) + new Date().getTimezoneOffset() * 1000 * 60;
     this.firstSelectedDay = this.todayTimestamp;
     this.secondSelectedDay = this.todayTimestamp + this.oneDay * 14;
@@ -64,22 +66,23 @@ class CustomCalendar{
         this.updateInput();
       });
     });
-    
-    this.firstInput.addEventListener('click', () => {
-      if (head_element.querySelector('.date_picker_calendar').classList.contains('hidden')){
-        head_element.querySelector('.date_picker_calendar').classList.remove('hidden')
-      }
-      if (!head_element.querySelector('.date_picker_input').classList.contains('showCal')){
-        head_element.querySelector('.date_picker_input').classList.add('showCal')
-      }
-    });
 
-    this.secondInput.addEventListener('click', () => {
-      if (head_element.querySelector('.date_picker_calendar').classList.contains('hidden')){
-        head_element.querySelector('.date_picker_calendar').classList.remove('hidden')
+    this._setInputBehavior();
+    this.updateInput();
+
+    this.clearSign.addEventListener('click', () => {
+      this._clearInputs();
+    });
+  }
+
+  _setInputBehavior(){
+    //first input
+    this.firstInput.addEventListener('click', () => {
+      if (this.head_element.querySelector('.date_picker_calendar').classList.contains('hidden')){
+        this.head_element.querySelector('.date_picker_calendar').classList.remove('hidden');
       }
-      if (!head_element.querySelector('.date_picker_input').classList.contains('showCal')){
-        head_element.querySelector('.date_picker_input').classList.add('showCal')
+      if (!this.head_element.querySelector('.date_picker_input').classList.contains('showCal')){
+        this.head_element.querySelector('.date_picker_input').classList.add('showCal')
       }
     });
 
@@ -87,11 +90,27 @@ class CustomCalendar{
       this.firstInput.classList.add('onFocus');
       this.secondInput.classList.contains('onFocus') && this.secondInput.classList.remove('onFocus');
     });
-    
+
     this.firstInput.addEventListener('blur',() =>{
       setTimeout(() =>  {
         this.firstInput.classList.remove('onFocus');
       }, 300);
+    });
+
+    this.firstInput.addEventListener('input', () => {
+      setTimeout(() => {
+        this._changeDateFromInput(this.firstInput, true);
+      }, 100);
+    });
+
+    //second Input
+    this.secondInput.addEventListener('click', () => {
+      if (this.head_element.querySelector('.date_picker_calendar').classList.contains('hidden')){
+        this.head_element.querySelector('.date_picker_calendar').classList.remove('hidden')
+      }
+      if (!this.head_element.querySelector('.date_picker_input').classList.contains('showCal')){
+        this.head_element.querySelector('.date_picker_input').classList.add('showCal')
+      }
     });
 
     this.secondInput.addEventListener('focus', ()=>{
@@ -105,32 +124,60 @@ class CustomCalendar{
       }, 300);
     });
 
-    this.firstInput.addEventListener('input', () => {
+    this.secondInput.addEventListener('input', () => {
       setTimeout(() => {
-      if (this.firstInput.value.replace("_", "").replace(/[а-яА-ЯёЁ]/g, "").replace(/\./g, "").replace(" ", "").length === 8){
-        let stringDate = this.firstInput.value.replace("_", "").replace(/[а-яА-ЯёЁ]/g, "").replace(" ", "");
-        let splittedDate = stringDate.split(".");
-        let date = Date.parse(splittedDate[2] + "-" + splittedDate[1] + "-" + splittedDate[0]);
-        if (!isNaN(date)){
-          console.log(splittedDate[0]);
-          this.year = Number(splittedDate[2]);
-          this.month = Number(splittedDate[1]) - 1;
-          this.setHeader(this.year, this.month);
-          console.log(this.monthDetails);
-          this.monthDetails = this.getMonthDetails(this.year, this.month);
-          console.log(this.monthDetails);
-          this.firstSelectedDay = date;
-          this.selectOnClick();
-          this.calendar.innerHTML = "";
-          this.setCalBody();
-          console.log();
-          this.setSelectedDate(Array.from(this.calendar.querySelectorAll('.cal_date.current')).find(el => el.querySelector("span").textContent === `${splittedDate[0][0] === "0" ? splittedDate[0][1] : splittedDate[0]}`));
-        }
-      }
+        this._changeDateFromInput(this.secondInput, false);
       }, 100);
     });
+  }
+  
+  _clearInputs(){
+    this.firstSelectedDay = null;
+    this.secondSelectedDay = null;
+    let days = this.calendar.querySelectorAll(".cal_date");
 
-    this.updateInput();
+    days.forEach(day => {
+      day.classList.remove("isSelected");
+      day.classList.remove("active");
+      day.classList.remove("additional-active");
+    });
+
+    this.firstInput.value = "";
+    this.secondInput.value = "";
+  }
+
+  _changeDateFromInput(input, isFirst){
+    if (input.value.replace("_", "").replace(/[а-яА-ЯёЁ]/g, "").replace(/\./g, "").replace(" ", "").length === 8){
+      let date = this._getTimeStampFromInput(input);
+      if (!isNaN(date)){
+        this.year = date.getFullYear();
+        this.month = date.getMonth();
+        date = date.getTime();
+        this.setHeader(this.year, this.month);
+        this.monthDetails = this.getMonthDetails(this.year, this.month);
+        if (isFirst && date < this.secondSelectedDay){
+          this.firstSelectedDay = date;
+        }
+        else if (!isFirst && date > this.firstSelectedDay){
+          this.secondSelectedDay = date
+        }
+        else if (!isFirst && date < this.firstSelectedDay){
+          this.secondInput = "по " + this.getDateStringFromTimestamp(this.firstSelectedDay);
+          this.firstSelectedDay = date;
+          this.firstInput.value = "с " + this.getDateStringFromTimestamp(date);
+        }
+        else if (isFirst && date > this.secondSelectedDay)
+        {
+          this.firstInput.value = "с " + this.getDateStringFromTimestamp(this.secondSelectedDay);
+          this.secondSelectedDay = date;
+          this.secondInput.value = "по " + this.getDateStringFromTimestamp(date);
+        }
+        this.selectOnClick();
+        this.calendar.innerHTML = "";
+        this.setCalBody();
+        this.updateInput();
+      }
+    }
   }
   
   getNumberOfDays(year, month) {
@@ -143,11 +190,11 @@ class CustomCalendar{
     let prevMonth = args.month - 1;
     let prevYear = args.year;
     if (prevMonth < 0) {
-    prevMonth = 11;
-    prevYear--;
+      prevMonth = 11;
+      prevYear--;
     }
     let prevMonthNumberOfDays = this.getNumberOfDays(prevYear, prevMonth);
-    let _date = (date < 0 ? prevMonthNumberOfDays + date : date % args.numberOfDays) + 1;
+    let _date =  (date < 0 ? prevMonthNumberOfDays + date : date % args.numberOfDays) + 1;
     let month = date < 0 ? -1 : date >= args.numberOfDays ? 1 : 0;
     let timestamp = new Date(args.year, args.month, _date).getTime();
     return {
@@ -161,12 +208,18 @@ class CustomCalendar{
   
   getMonthDetails(year, month) {
     let firstDay = new Date(year, month).getDay();
+    console.log(firstDay);
     let numberOfDays = this.getNumberOfDays(year, month);
     let monthArray = [];
     let rows = 5;
+    this.calendar.style["grid-template-rows"] = "repeat(5, min(30px))";
     let currentDay = null;
     let index = 0;
     let cols = 7;
+    if ((firstDay >= 5 && numberOfDays === 31) || (firstDay >= 6 && numberOfDays === 30)){
+      rows = 6;
+      this.calendar.style["grid-template-rows"] = "repeat(6, min(30px))";
+    }
     for (let row = 0; row < rows; row++) {
       for (let col = 0; col < cols; col++) {
         currentDay = this.getDayDetails({
@@ -182,13 +235,8 @@ class CustomCalendar{
     }
     return monthArray;
   }
-  
-  isCurrentDay(day, cell, isFirst){
-    this.setSelectedDate(cell);
-  }
 
   setMiddleSelected(){
-    console.log(this.head_element.querySelectorAll(".cal_date.isSelected"));
     if (this.head_element.querySelectorAll(".cal_date.isSelected").length === 2){
       let days = this.calendar.querySelectorAll(".cal_date");
       let fromChecker = false;
@@ -206,10 +254,34 @@ class CustomCalendar{
         }
       });
     }
-  }
-
-  removeMiddleSelected(){
-
+    else if (this.head_element.querySelectorAll(".cal_date.isSelected").length === 1 && this._checkInputFillnes(this.firstInput) && this._checkInputFillnes(this.secondInput)){
+      let activeDate = this.head_element.querySelector(".cal_date.isSelected");
+      let activeDateTimestamp = this._getCellTimestamp(activeDate);
+      let days = this.calendar.querySelectorAll(".cal_date.current");
+      let fromChecker = true;
+      if (activeDateTimestamp === this.firstSelectedDay) {fromChecker = false}
+      days.forEach(day => {
+        if (day === activeDate){
+          fromChecker = !fromChecker;
+        }
+        else if (fromChecker){
+          day.classList.add("additional-active");
+        }
+        else{
+          day.classList.remove("additional-active");
+        }
+      });
+    }
+    else if (this.head_element.querySelectorAll(".cal_date.isSelected").length === 0 && this._checkInputFillnes(this.firstInput) && this._checkInputFillnes(this.secondInput)){
+      let firstDate = new Date(this.firstSelectedDay);
+      let secondDate = new Date(this.secondSelectedDay);
+      if (this.year <= secondDate.getFullYear() && this.year >= firstDate.getFullYear() && this.month <= secondDate.getMonth() && this.month >= firstDate.getMonth()){
+        let days = this.calendar.querySelectorAll(".cal_date.current");
+        days.forEach(day => {
+          day.classList.add("additional-active");
+        });
+      }
+    }
   }
   
   setSelectedDate(cell){
@@ -259,21 +331,33 @@ class CustomCalendar{
   }
   
   setCalBody(){
+    let firstSelected;
+    let secondSelected;
     for (let i = 0; i < this.monthDetails.length; i++) {
         let div = document.createElement("div");
         let span = document.createElement("span");
         div.classList.add("cell_wrapper");
         div.classList.add("cal_date");
         this.monthDetails[i].month === 0 && div.classList.add("current");
+        //if (this.monthDetails[i].timestamp === this.todayTimestamp) {div.classList.add("inactive_indicator");}
         span.classList.add("cell_item");
-      
         span.innerText = this.monthDetails[i].date;
-      
         div.appendChild(span);
         this.calendar.appendChild(div);
-        (this.monthDetails[i].timestamp === this.firstSelectedDay && this.monthDetails[i].month === 0)  && this.isCurrentDay(this.monthDetails[i], div, true);
-        (this.monthDetails[i].timestamp === this.secondSelectedDay && this.monthDetails[i].month === 0) && this.isCurrentDay(this.monthDetails[i], div, false);
-        
+        if (this.monthDetails[i].timestamp === this.firstSelectedDay && this.monthDetails[i].month === 0) {firstSelected = div;}
+        if (this.monthDetails[i].timestamp === this.secondSelectedDay && this.monthDetails[i].month === 0) {secondSelected = div;}
+    }
+
+    if (firstSelected !== undefined){
+      this.setSelectedDate(firstSelected);
+    }
+
+    if (secondSelected !== undefined){
+      this.setSelectedDate(secondSelected);
+    }
+
+    if (secondSelected === undefined && firstSelected === undefined){
+      this.setMiddleSelected();
     }
   }
   
@@ -309,13 +393,13 @@ class CustomCalendar{
   setDateToInput(timestamp, input){
     let dateString = this.getDateStringFromTimestamp(timestamp);
     if (input == this.firstInput) dateString = "с " + dateString;
-    if (input == this.secondInput) dateString = "с " + dateString;
+    if (input == this.secondInput) dateString = "по " + dateString;
     input.value = dateString;
   }
 
   getDateStringFromTimestamp(timestamp){
     let dateObject = new Date(timestamp);
-    let month = ("0" + (dateObject.getMonth() + 1));
+    let month = dateObject.getMonth() > 9 ? dateObject.getMonth() + 1: ("0" + (dateObject.getMonth() + 1));
     let date = ("0" + dateObject.getDate()).slice(-2);
     return `${date}.${month}.${dateObject.getFullYear()}`;
   }
@@ -339,6 +423,7 @@ class CustomCalendar{
                 else{
                   this.secondSelectedDay = this.monthDetails[i].timestamp;
                   this.setDateToInput(this.secondSelectedDay, this.secondInput);
+                  this._closeCalendar();
                 }
                 this.selectOnClick();
                 this.setSelectedDate(cell);
@@ -354,8 +439,8 @@ class CustomCalendar{
   }
 
   checkDateSelectionDirection(cellTimeStamp){
-    return !(cellTimeStamp >= this.secondSelectedDay && this.secondSelectedDay != null) &&
-          ((cellTimeStamp <= this.firstSelectedDay && this.firstSelectedDay != null && this.secondSelectedDay != null) ||
+    return !(cellTimeStamp >= this.secondSelectedDay && this.secondSelectedDay !== null) &&
+          ((cellTimeStamp <= this.firstSelectedDay && this.firstSelectedDay !== null && this.secondSelectedDay !== null) ||
           this.firstInput.value.replace("_", "").replace(/[а-яА-ЯёЁ]/g, "").replace(/\./g, "").replace(" ", "").length === 0 || 
           this.firstInput.classList.contains('onFocus'));
   }
@@ -363,6 +448,29 @@ class CustomCalendar{
   _getCellTimestamp(cell){
     let day = cell.querySelector("span").innerHTML;
     return new Date(this.year, this.month, day).getTime();
+  }
+
+  _checkInputFillnes(input){
+    return input.value.replace("_", "").replace(/[а-яА-ЯёЁ]/g, "").replace(/\./g, "").replace(" ", "").length === 8;
+  }
+
+  _getTimeStampFromInput(input){
+    let stringDate = input.value.replace("_", "").replace(/[а-яА-ЯёЁ]/g, "").replace(" ", "");
+    let splittedDate = stringDate.split(".");
+    let date = Date.parse(splittedDate[2] + "-" + splittedDate[1] + "-" + splittedDate[0]);
+    if (!isNaN(date)){
+      return new Date(Number(splittedDate[2]), Number(splittedDate[1]) - 1,splittedDate[0][0] === "0" ?  Number(splittedDate[0][1]) : Number(splittedDate[0]));
+    }
+    else {
+      return NaN;
+    }
+  }
+
+  _closeCalendar(){
+    setTimeout(() => {
+      this.head_element.querySelector('.date_picker_calendar').classList.add('hidden');
+      this.head_element.querySelector('.date_picker_input').classList.remove('showCal');
+    }, 1000);
   }
 }
   
